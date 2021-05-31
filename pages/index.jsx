60a6e2e6e36c242c2ef7badb81377f2c89431ea4
locomoco <eren.karahasan@web.de>
2021-05-31 17:43:18 +0000

    Forgot to push

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
 import useSWR from 'swr'
+import Checkin from '../components/checkin'
+import CheckInOut from '../components/checkinout'
 
 
 
 export default function Index() { 
-    const [checkin, setCheckin] = React.useState(1)
+    const [checkin, setCheckin] = React.useState(-1)
     const [popup, setPopup] = React.useState(false)
+    const [notOccupied, setnotOccupied] = React.useState(false)
 
     const [aussie, setAussie] = React.useState(false)
-
-    React.useEffect(() => {
-        document.body.style.transform = aussie ? "rotate(2deg)" : ""
-    }, [aussie])
 
     const _res = useSWR("/api/rooms")
 
@@ -28,6 +27,25 @@ function getNextFreeRoom(data){
     foundInEtage = etage.find(zimmer => !zimmer.occupied)
     return (typeof foundInEtage != "undefined") 
   }) ? "es wurde kein freies Zimmer gefunden": foundInEtage
+}
+
+function checkInAdvanced(data) {
+    const zimmerNo = checkin
+
+    fetch('/api/rooms', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({roomNo: checkin, occupy: true, name: `${data.Nachname}, ${data.Vorname}` , address: `${data.Strasse} ${data.Hausnr}, ${data.Stadt}`})
+  }).then(resp => {
+      console.log({resp})
+  }).catch(err => {
+      console.log({err})
+  })
+
+    setCheckin(-1)
 }
 
 function einchecken(zimmer){
@@ -49,6 +67,24 @@ function einchecken(zimmer){
       console.log({err})
   })
   */
+}
+function auscheckenByNumber(etage,raum){
+  let nullEtage = to3DigitStr(etage+raum)
+  let raumNo = etage == 0 ? nullEtage: etage*10+raum
+    fetch('/api/rooms', {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({roomNo: raumNo,occupy: false})
+  }).then(resp => {
+      console.log({resp})
+  }).catch(err => {
+      setnotOccupied(true)
+       console.log({err})
+  })
+  console.log(notOccupied)
 }
 
 function auschecken(zimmer){
@@ -75,33 +111,98 @@ function auschecken(zimmer){
     }}> 
       <button style={{ height:"2.5rem", width:"50%"}} onClick={() => {
           console.log(getNextFreeRoom(res.data))
+          einchecken(getNextFreeRoom(res.data))
       }}>Automatisch Buchen</button>
       <button style={{ height:"2.5rem",  width:"50%"}} onClick={() => {
         {popup == false ? setPopup(true): setPopup(false);console.log(popup)}
-      }}>auschecken</button>
+      }}>{popup ? "abbrechen" : "auschecken"}</button>
     </div>
 
     {popup == true ? <div 
       
       style={{ 
-        justifyContent:"space-evenly",
+        justifyContent:"space-around",
         padding:"2rem",
         display: "flex",
-        
-        flexFlow:"row wrap", position: "absolute", width: "50%", height: "20%",background: "#888",zIndex: "10", margin:"0,auto", left:"50%",transform: "translateX(-50%)",top:"10%"}}>
-<form style={{ display: "flex", justifyContent: "space-evenly" }}>
-  <span style={{ flexBasis: "50%",
+        position: "fixed", 
+        flexDirection:"row",
+        flexWrap:"wrap",
+        width: "50%", 
+        height: "30%",
+        background: "#888",
+        zIndex: "10", 
+        margin:"0,auto", 
+        left:"50%",
+        transform: "translateX(-50%)",
+        top:"10%"}}>
+<form style={{ flexBasis:"100%", display: "flex", flexWrap:"wrap",flexDirection: "row" }}>
+  <span style={{ flexBasis: "100%",
                  display: "flex", flexDirection: "column" }}>
-    <label for="level"style={{ height:"1.5rem"}}>Etage</label>
-    <input id="level" name="level" style={{ height:"2.5rem"}} />
+    <label style={{ fontSize:"1.5rem", textAlign:"center",height:"1.5rem"}}>Etage</label>
+  <select id="level" name="level" style={{ height:"2.5rem"}}>
+    <option value="0">Erdgeschoss (EG)</option>
+    <option value="1">Erste Etage (1.OG)</option>
+    <option value="2">Zweite Etage (2.OG)</option>
+    <option value="3">Dritte Etage (3.OG)</option>
+    <option value="4">Vierte Etage (4.OG)</option>
+    <option value="5">Fünfte Etage (5.OG)</option>
+    <option value="6">Sechste Etage (6.OG)</option>
+  </select>
+  </span>
+    <span style={{ flexBasis: "100%",
+                 display: "flex", flexDirection: "column",height:"1rem"}}>
+
   </span>
 
-  <span style={{ flexBasis: "50%",
-                 display: "flex", flexDirection: "column" }}>
-    <label for="roomNo"style={{ height:"1.5rem"}}>Zimmer Nummer</label>
-    <input id="roomNo" name="roomNo" style={{ height:"2.5rem"}}/>
+  <span style={{ flexBasis: "100%",
+                 display: "flex", flexDirection: "column"}}>
+    <label style={{ textAlign:"center",height:"1.5rem",fontSize:"1.5rem",}}>Zimmer</label>
+      <select id="roomNo" name="roomNo" style={{ height:"2.5rem"}}>
+    <option value="1">Zimmer 1</option>
+    <option value="2">Zimmer 2</option>
+    <option value="3">Zimmer 3</option>
+    <option value="4">Zimmer 4</option>
+    <option value="5">Zimmer 5</option>
+    <option value="6">Zimmer 6</option>
+    <option value="7">Zimmer 7</option>
+    <option value="8">Zimmer 8</option>
+    <option value="9">Zimmer 9</option>
+    <option value="10">zimmer 10</option>
+  </select>
   </span>
 </form>
+<span style={{ flexBasis: "100%", display: "flex"}}>
+  <button style={{height:"2rem",flexBasis: "100%" }}  onClick={() => {
+    {auscheckenByNumber(document.getElementById('level').value,document.getElementById('roomNo').value)}
+    }
+    }
+  ><b style={{textAlign:"center"}}>auschecken</b></button>
+  </span>
+    </div> : ""}
+     {notOccupied == true ? <div 
+      
+      style={{ 
+        justifyContent:"space-around",
+        padding:"2rem",
+        display: "flex",
+        position: "fixed", 
+        flexDirection:"row",
+        flexWrap:"wrap",
+        width: "30%", 
+        height: "10%",
+        background: "#888",
+        zIndex: "20", 
+        margin:"0,auto", 
+        left:"50%",
+        transform: "translateX(-50%)",
+        top:"10%"}}>
+<span style={{ flexBasis: "100%", display: "flex"}}>
+  <button style={{height:"2rem",flexBasis: "100%" }}  onClick={() => {
+    {auscheckenByNumber(document.getElementById('level').value,document.getElementById('roomNo').value)}
+    }
+    }
+  ><b style={{textAlign:"center"}}>Okay</b></button>
+  </span>
     </div> : ""}
     
     {checkin >= 0 && <div style={{position: "fixed", maxWidth: "15rem", left: "50%", top: "10%", zIndex: "10", transform: "translateX(-50%)", background: "#888", minHeight: "5rem", padding: "2rem 5rem", borderRadius: "15px"
@@ -109,78 +210,10 @@ function auschecken(zimmer){
     // hier ist die box yeee
     }}
     >
-         <form style={{ 
-                width: "100%", // passt sich an den parent an, der ist responsive, nice (doch noch nicht, braucht bissl verbesserung xD) lmao
-                 display: "flex",
-                 flexDirection: "column",
-                 
-                 }}>
-                
-                 <h1> Was geht bruder, check ein 💯</h1>
-                 <img style={{maxHeight: "180px", display: aussie ? "none" : "block"}} src="https://media1.tenor.com/images/0f36726b6c2ee4e6c9ff0acf61952ef3/tenor.gif?itemid=21357175" />
-                 {aussie && <h2>KAPUTT</h2>}
-                 <img style={{maxHeight: "180px", display: aussie ? "block" : "none"}} src="https://media1.tenor.com/images/cb6a0257a5a89e3d0ad6f9a399f0ce10/tenor.gif?itemid=20437504" />
-                 
-                <label for="Vorname"> Vorname:</label>
-                <input type="text" id="Vorname" name="Vorname"/> 
-                <label for="Nachname"> Nachname:</label>
-                <input type="text" id="Nachname" name="Nachname"/> 
-                 <label for="Stadt"> Stadt:</label> 
-                <input type="text" id="Stadt" name="Stadt"/> 
-                 <label for="Straße"> Straße:</label> 
-                <input type="text" id="Straße" name="Straße"/> 
-                 <label for="Hausnr."> Hausnr.:</label> 
-                <input type="number" id="Hausnr." name="Hausnr."/> 
-                <span>
-                <button onClick={(e) => {
-                    e.preventDefault()
-                }}>Einchecken</button>
-                <button onClick={(e) => {
-                    e.preventDefault()
-                    setAussie(!aussie)
-                    setCheckin(-1)
-                }}>Abbrechen</button> 
-                </span>
-         </form>
+         <Checkin onSubmit={checkInAdvanced} zimmer={checkin} setAussie={setAussie} setCheckin={setCheckin} aussie={aussie} />
     </div>}
 
-    <div style={{
-        width: "100%",
-        display: "flex",
-        flexFlow: "column wrap",
-        background: "#888",
-        gap: "1rem"
-    }}> 
-      {res.data.map((etage, index) => <div style={{
-        display: "flex",
-        flexFlow: "column nowrap",
-        background: "#333",
-        padding:"0.5rem"
-      }}><h2 style={{color:"white"}}>{index}</h2>
-      <div style={{
-        width:"100%",
-        display:"flex",
-        flexFlow:"row wrap",
-        alignItems:"space-evenly",
-        gap: "1rem",
-        padding: "1rem"
-      }}>
-      {etage.map((zimmer, index) => <div style={{flex: "0 0 15rem",padding:"0.5rem",background: "#DDD", position:"relative",borderRadius: "3px 15px 5px 30px"}}>
-        <h3>Zimmer {to3DigitStr(zimmer.roomNo)}</h3>
-        <p>{zimmer.occupied ? "Zimmer ist Besetzt" : " Zimmer ist noch Frei"}</p>
-        <div style={{width:"2rem", height: "2rem", borderRadius:"50%", position:"absolute", backgroundColor:zimmer.occupied ? "red" : "green", top: "1.5rem", right:"1.5rem"}}> </div>
-        <button style={{width:"4,6rem", height: "2rem", borderRadius: "5px 5px 5px 5px", position:"absolute", top: "4.5rem", right:"0.8rem",background: "#888"}}
-        onClick={() => {
-          {zimmer.occupied ? auschecken(zimmer)  : einchecken(zimmer)}
-        }}>
-          <b>
-          {zimmer.occupied ? "auschecken" : "einchecken"}
-          </b>
-        </button>
-      </div>)}
-      </div>
-      </div>)}
-    </div>  
+    <CheckInOut data={res.data} auschecken={auschecken} einchecken={einchecken} />
     </>
 }
 
